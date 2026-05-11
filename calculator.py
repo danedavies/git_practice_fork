a397b7b1d1af4ec64771ea928c05cc0bfc8eaf4c
rweir136 <raw5878@psu.edu>
2026-05-11 15:47:34 -0400

    add mult

--- calculator.py
+++ calculator.py
@@ -7,6 +7,9 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
 if __name__ == "__main__":
     print("Calculator loaded!")
     print(f"5 + 3 = {add(5, 3)}")
